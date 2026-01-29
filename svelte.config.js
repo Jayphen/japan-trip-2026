69047ee0deaf38ab8a2207dd7109e44103f15b17
Jayphen <software@jayphen.com>
2026-01-29 15:52:26 +1100

    Fix Cloudflare Pages: Add .svelte-kit/cloudflare to git (not ignored)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +6,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Cloudflare Pages has a number of reserved "_" paths (_headers, _redirects, _routes.json, etc).
-		// Using the default SvelteKit appDir of "_app" can lead to assets not being served.
-		// Rename it to avoid collisions.
 		appDir: 'app',
 		adapter: adapter()
 	}
